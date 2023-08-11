ee820710790bdcd6c567203c0238c229d6da6282
Reyna May <roma.reynamayA@gmail.com>
2023-08-10 19:12:05 -0600

    Modify HomeRoute to store information for faves

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ topics, photos }) => {
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+
+  const toggleFavorite = (photoId) => {
+    if (favoritedPhotos.includes(photoId)) {
+      setFavoritedPhotos(favoritedPhotos.filter((id) => id !== photoId));
+    } else {
+      setFavoritedPhotos([...favoritedPhotos, photoId]);
+    }
+  }
+
   return (
     <div className="home-route">
       {/* Insert React */}
-      <TopNavigation topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigation
+      topics={topics}
+      favoritedPhotos={favoritedPhotos}
+      />
+      <PhotoList
+      photos={photos}
+      favoritedPhotos={favoritedPhotos}
+      toggleFavorite={toggleFavorite}
+      />
     </div>
   );
 };

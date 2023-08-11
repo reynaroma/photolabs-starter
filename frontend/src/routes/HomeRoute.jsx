ed644d37cc61837258fb8e54ea816b12964f5834
Reyna May <roma.reynamayA@gmail.com>
2023-08-10 19:24:47 -0600

    Add new file to mage favorite photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,33 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
+import { FavoritesProvider } from 'components/FavoriteContext';
 
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ topics, photos }) => {
-  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
-
-  const toggleFavorite = (photoId) => {
-    if (favoritedPhotos.includes(photoId)) {
-      setFavoritedPhotos(favoritedPhotos.filter((id) => id !== photoId));
-    } else {
-      setFavoritedPhotos([...favoritedPhotos, photoId]);
-    }
-  }
 
   return (
-    <div className="home-route">
-      {/* Insert React */}
-      <TopNavigation
-      topics={topics}
-      favoritedPhotos={favoritedPhotos}
-      />
-      <PhotoList
-      photos={photos}
-      favoritedPhotos={favoritedPhotos}
-      toggleFavorite={toggleFavorite}
-      />
-    </div>
+    <FavoritesProvider>
+      <div className="home-route">
+        {/* Insert React */}
+        <TopNavigation
+          topics={topics}
+        />
+        <PhotoList
+          photos={photos}
+        />
+      </div>
+    </FavoritesProvider>
   );
 };
 

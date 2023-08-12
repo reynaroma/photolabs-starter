5d14b239eabc5ab8ee98038ff9201890278f602d
Reyna May <roma.reynamayA@gmail.com>
2023-08-11 21:30:52 -0600

    Display the large photo in the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,18 +4,27 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ isOpen, onClose, selectedPhoto }) => {
-  if (!isOpen) return null;
+  if (!isOpen || !selectedPhoto) return null;
   console.log(selectedPhoto);
 
+  const similarPhotos = selectedPhoto.similar_photos;
+  const similarPhotoKeys = Object.keys(similarPhotos);
   return (
     <div className="photo-details-modal">
       <button
-      className="photo-details-modal__close-button" onClick={onClose}>
+        className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div>
-        {/* <img src={selectedPhoto.}/> */}
+        <img
+          className='photo-details-modal__image'
+          src={selectedPhoto.urls.full}
+        />
+        <p className='photo-details-modal__header'>
+          Similar Photos
+        </p>
       </div>
+
     </div>
   )
 };
